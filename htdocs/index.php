75dd20072a8d0af020766a61e44a7a5827c9f443
RobsonMourasz <robsonic10@gmail.com>
2025-03-17 14:20:46 -0300

    first commit

---
+++ htdocs/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/assets/css/login.css">
+    <title>Login - Parametrização</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="login">
+
+            <div class="page section-login">
+                <div class="title">
+                    <h1>Faça seu login</h1>
+                </div>
+                <div class="content">
+
+                    <form action="" method="post">
+                        <div class="group-input">
+                            <label for="" class="label-input">Usuario</label>
+                            <input type="text" name="user" id="user" class="input-label">
+                        </div> <!-- group-input -->
+
+                        <div class="group-input">
+                            <label for="" class="label-input">Senha</label>
+                            <input type="password" name="user" id="user" class="input-label">
+                        </div> <!-- group-input -->
+
+                        <div class="group-input-check">
+                            <input type="checkbox" id="check-lembrar">
+                            <label for="check-lembrar">  Me lembre novamente  </label>
+                        </div> <!-- group-input -->
+
+                        <div class="group-input"> 
+                            <input type="submit" value="Acessar" class="btn btn-login">
+                        </div> <!-- group-input -->
+
+                    </form>
+
+                </div>
+            </div> <!-- section-login -->
+
+            <div class="page section-img">
+                <div class="img">
+                    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEivJI9f7wTW8lbzp46y6P1-NWYwIGLahELLvqQWvNLcnys1sqbwoaKYh0qpcIvas-V6Uiz6ElckCzNJeCxjbKjWgLEfKzxQb6hS-m6qkTCufkDYv-GTAwQb13NDHFaHUlbwhKNUFhvRHxs/s1600/bem-vindo-bem-vindo-ou-benvindo.png" alt="Bem vindo " width="300">
+                </div>
+            </div> <!-- section-img -->
+
+        </div> <!-- login -->
+
+    </div> <!-- container -->
+    <script src="app/assets/js/login.min.js"></script> <!-- JS Efeitos  -->
+</body>
+
+</html>
