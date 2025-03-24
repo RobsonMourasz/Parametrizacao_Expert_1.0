fb67a6fc8f3168a5803d06a80dea38e93444f066
RobsonMourasz <robsonic10@gmail.com>
2025-03-24 18:24:40 -0300

    Refatora o layout da página de parametrização, adiciona novos estilos CSS e melhora a estrutura do formulário de pesquisa

--- htdocs/public/ADM/index.php
+++ htdocs/public/ADM/index.php
@@ -4,51 +4,85 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../app/assets/css/style.css">
+    <link rel="stylesheet" href="../../app/assets/css/adm.css">
     <title>ADM - Expert</title>
 </head>
 
-<body>
-    <h1>Lista de Parametrização</h1>
-    <div class="pesquisa">
-        <input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquisar">
-        <button>Pesquisar</button>
-        <button>Adicionar</button>
-        <button>Visualizar</button>
-    </div>
-    <div class="empresas">
-        <div class="table">
+<body style="background-color: #f4f4f4;">
 
-            <div class="table-header">
-                <div class="table-row">
-                    <div class="table-cell">Nome Empresa</div>
-                    <div class="table-cell">CNPJ</div>
-                    <div class="table-cell">Status</div>
-                </div> <!-- table-row -->
-            </div> <!-- table-header -->
+    <div class="container">
+        <div class="container-page">
+            <div class="header">
 
-            <div class="table-body">
+                <div class="titulo">
+                    <h1>Lista de Parametrização</h1>
+                </div> <!--titulo -->
 
-                <div class="table-row">
-                    <div class="table-cell">HS BEBIDAS</div>
-                    <div class="table-cell">56.991.836/0001-50</div>
-                    <div class="table-cell">Preenchido</div>
-                </div> <!-- table-row -->
 
-                <div class="table-row">
-                    <div class="table-cell">MR ELETRICA AUTOMOTIVA/ROMES JOSE DE OLIVEIRA</div>
-                    <div class="table-cell">58.654.857/0001-05</div>
-                    <div class="table-cell">Preenchido</div>
-                </div> <!-- table-row -->
+                <div class="pesquisa">
+                    <form action="" method="post">
+                        <div class="row center">
+                            <div class="group-input w-50">
+                                <input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquisar">
+                            </div>
+                            <div class="group-botoes w-30 align-left">
+                                <input type="submit" name="pesquisar" id="pesquisar" class="btn btn-primary" value="pesquisar">
+                            </div>
+                        </div>
+                    </form>
+                    <div class="col">
+                        <div class="group-botoes justify-content-left" >
+                            <button class="btn btn-success">Adicionar</button>
+                            <button class="btn btn-info">Visualizar</button>
+                        </div>
+                    </div>
+                </div><!--pesquisa-->
 
-            </div> <!-- table-body -->
 
-        </div>
-    </div> <!-- empresas -->
+            </div> <!-- header -->
+        </div> <!--container-page-->
+
+
+
+        <div class="container-page">
+            <div class="empresas">
+                <div class="table">
+
+                    <div class="table-header">
+                        <div class="table-row">
+                            <div class="table-cell">Nome Empresa</div>
+                            <div class="table-cell">CNPJ</div>
+                            <div class="table-cell">Status</div>
+                        </div> <!-- table-row -->
+                    </div> <!-- table-header -->
+
+                    <div class="table-body">
+
+                        <div class="table-row">
+                            <div class="table-cell">HS BEBIDAS</div>
+                            <div class="table-cell">56.991.836/0001-50</div>
+                            <div class="table-cell">Preenchido</div>
+                        </div> <!-- table-row -->
+
+                        <div class="table-row">
+                            <div class="table-cell">MR ELETRICA AUTOMOTIVA/ROMES JOSE DE OLIVEIRA</div>
+                            <div class="table-cell">58.654.857/0001-05</div>
+                            <div class="table-cell">Preenchido</div>
+                        </div> <!-- table-row -->
+
+                    </div> <!-- table-body -->
+
+                </div> <!-- table -->
+            </div> <!-- empresas -->
+        </div> <!-- container-page -->
+    </div> <!-- container -->
+
 
 
     <!-- TELA MODAL -->
 
-    <div class="modal" id="modal-empresa">
+    <div class="modal d-none" id="modal-empresa">
 
         <div class="modal-content">
 
@@ -78,10 +112,10 @@
 
     <!-- FIM TELA MODAL -->
 
-    
+
     <!-- TELA MODAL -->
 
-    <div class="modal" id="modal-empresa">
+    <div class="modal d-none" id="modal-empresa">
 
         <div class="modal-content">
 
@@ -111,7 +145,7 @@
 
     <!-- FIM TELA MODAL -->
 
-    
+
 </body>
 
 </html>
